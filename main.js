5c1f5686782b1ea381292cda809dd3b28ac21c9d
Yael Matzek <jessianeroque@gmail.com>
2025-10-28 14:31:31 +0200

    Change onBallClick to increase width & height by 50px &  show the new diameter inside the ball

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 'use strict'
 
-function onBallClick(this){
-    console.log('clicked')
+function onBallClick(elBall){
+
+    var currWidth = parseInt(elBall.clientWidth)
+    var currHeight = parseInt(elBall.clientWidth)
+
+    var newWidth = currWidth + 50
+    var newHeight = currHeight + 50
+
+    elBall.style.width = newWidth + 'px'
+    elBall.style.height = newHeight + 'px'
+
+    elBall.innerText = newWidth
 }
