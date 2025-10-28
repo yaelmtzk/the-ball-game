77c2a95d79b4ba15288bda4e7e5d69766868c9a7
yaelmtzk <jessianeroque@gmail.com>
2025-10-28 17:00:42 +0200

    Create getRandomColor function

--- util.js
+++ util.js
@@ -3,3 +3,12 @@ function getRandIntInclusive(min, max) {
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
