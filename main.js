29719c1ccb41f20ab0cf0945a08f9ee46e05db85
yaelmtzk <jessianeroque@gmail.com>
2025-10-28 19:15:54 +0200

    Set onBall3Click() to swap sizes and colors of balls

--- main.js
+++ main.js
@@ -38,3 +38,29 @@ function changeBallSize(elBall, maxDiameter){
 function changeBallColor(elBall){
     elBall.style.backgroundColor = getRandomColor()
 }
+
+function onBall3Click(){
+
+    var elBall1 = document.querySelector('.ball1')
+    var elBall2 = document.querySelector('.ball2')
+
+    console.log(currSize1);
+
+    var currSize1 = parseInt(elBall1.clientWidth)
+    var currSize2 = parseInt(elBall2.clientWidth)
+
+    elBall1.style.width = currSize2 + 'px'
+    elBall1.style.height = currSize2 + 'px'
+
+    elBall2.style.width = currSize1 + 'px'
+    elBall2.style.height = currSize1 + 'px'
+
+    elBall1.innerText = currSize2
+    elBall2.innerText = currSize1
+
+    var currBackColor1 = getComputedStyle(elBall1).backgroundColor
+    var currBackColor2 = getComputedStyle(elBall2).backgroundColor
+
+    elBall1.style.backgroundColor = currBackColor2
+    elBall2.style.backgroundColor = currBackColor1
+}
