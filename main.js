e0f4e326f57a45d2c282c5d4f939a775f5ebea49
Yael Matzek <jessianeroque@gmail.com>
2025-10-28 15:27:27 +0200

    Change onBallClick to max 400px  size & Shrink to 100px if reached limit

--- main.js
+++ main.js
@@ -5,8 +5,17 @@ function onBallClick(elBall){
     var currWidth = parseInt(elBall.clientWidth)
     var currHeight = parseInt(elBall.clientWidth)
 
-    var newWidth = currWidth + 50
-    var newHeight = currHeight + 50
+    var newWidth
+    var newHeight
+
+    if(currWidth < 400){
+        newWidth = currWidth + 50
+        newHeight = currHeight + 50
+    }
+    else{
+        newWidth = 100
+        newHeight = 100
+    }
 
     elBall.style.width = newWidth + 'px'
     elBall.style.height = newHeight + 'px'
